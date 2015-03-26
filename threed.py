92fef62c807eda1c0ba8331111ff7076af0b5813
Niklaus Johner <niklaus.johner@unibas.ch>
2015-03-26 09:02:57 +0100

    Added Marco's threed.py module, which allows to build up a scene from python.

---
+++ threed.py
@@ -0,0 +1,118 @@
+"""
+Module written by Marco Biasini.
+
+This module contains functions to startup a scene from python.
+"""
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtOpenGL import *
+try:
+  from ost import gfx
+except ImportError:
+  print 'can not find openstructure module. make sure it is in your python path'
+
+import weakref
+
+class _GLWin(gfx.GLWinBase):
+  """
+  We need this class to get proper redraw events when the scene changes.
+  """
+  def __init__(self, threed_widget):
+    gfx.GLWinBase.__init__(self)
+    self._threed_widget = weakref.proxy(threed_widget)
+    gfx.Scene().Register(self)
+
+  def DoRefresh(self):
+    self._threed_widget.update()
+  def Height(self): return self.threed_widget.height()
+  def Width(self): return self.threed_widget.width()
+
+class ThreeDWidget(QGLWidget):
+  def __init__(self, format, parent=None):
+    QGLWidget.__init__(self, format, parent=None)
+    self._last_pos = QPoint()
+    self._gl_win = _GLWin(self)
+    self._key_handler = {}
+  def initializeGL(self):
+    gfx.Scene().InitGL(True)
+    gfx.Scene().fogno=10
+
+  def paintGL(self):
+    gfx.Scene().SetShadingMode('hemilight')
+    gfx.Scene().RenderGL()
+
+  def resizeGL(self, w, h):
+    gfx.Scene().Resize(w, h)
+
+  def HandleKey(self, key, handler):
+    self._key_handler[key] = handler
+
+  def keyPressEvent(self, event):
+    handler = self._key_handler.get(str(event.text()), None)
+    if handler:
+      handler()
+
+  def mousePressEvent(self, event):
+    self._last_pos = QPoint(event.x(), event.y())
+    gfx_obj, atom=gfx.PickAtom(gfx.Scene(), event.x(),
+                               self.height()-event.y())
+    if gfx_obj:
+      print 'picked: ', gfx_obj.name, atom
+  def mouseMoveEvent(self, event):
+    """
+    Handles translation and rotation of the scene...
+    """
+    delta = QPoint(event.x(), event.y()) - self._last_pos
+    if event.buttons() & Qt.LeftButton:
+      self._RotateDelta(delta)
+
+    self._last_pos = QPoint(event.x(), event.y())
+  def wheelEvent(self, event):
+    """
+    Scrolling zooming is...
+    """
+    gfx.Scene().Apply(gfx.InputEvent(gfx.INPUT_DEVICE_MOUSE,
+                                     gfx.INPUT_COMMAND_TRANSZ,
+                                     -0.1*event.delta()), False)
+    self.update()
+
+  def _RotateDelta(self, delta):
+    """
+    Responds to a change in mouse position of delta.
+    """
+    if delta.y()!=0:
+      gfx.Scene().Apply(gfx.InputEvent(gfx.INPUT_DEVICE_MOUSE,
+                                       gfx.INPUT_COMMAND_ROTX,
+                                       delta.y()*0.5), False)
+    if delta.x()!=0:
+      gfx.Scene().Apply(gfx.InputEvent(gfx.INPUT_DEVICE_MOUSE,
+                                       gfx.INPUT_COMMAND_ROTY,
+                                       delta.x()*0.5), False)
+    self.update()
+
+def App(title='OST Viewer', width=800, height=600):
+  """
+  """
+  import sys
+  app=QApplication(sys.argv)
+  fmt=QGLFormat()
+  fmt.setAlpha(True)
+  fmt.setSampleBuffers(True)
+  wnd=ThreeDWidget(fmt)
+  wnd.resize(width, height)
+  wnd.setWindowTitle(title)
+  wnd.show()
+  app.threed=wnd
+  return app
+if __name__ == '__main__':
+  from ost import io
+  # example use
+  app  = App()
+  structure = io.LoadPDB('1crn.pdb')
+  obj = gfx.Entity('structure', gfx.HSC, structure)
+  obj.SetColor(gfx.YELLOW, 'rtype=H,E')
+  obj.SetDetailColor(gfx.RED, 'rtype=H,E')
+  gfx.Scene().Add(obj)
+  app.exec_()
+
