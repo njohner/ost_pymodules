07f55811cfb495b60604de9d172f3d81dba4e496
Niklaus Johner <niklaus.johner@unibas.ch>
2015-08-03 10:53:36 +0200

    Added function to create PSSM matrix from an AlignmentHandle.

--- sequence_alg.py
+++ sequence_alg.py
@@ -74,4 +74,40 @@ def RandomizeAlignment(ali,fix_gaps=True):
     ali2.AddSequence(s2)
   return ali2
 
+class PSSM:
+  """
+  Class to create a position specific scoring matrix from a multiple sequence alignment.
+  The object can then be used to score a sequence.
+  """
+  def __init__(self,ali):#,aa_list=None,pseudocounts=None,aa_frequencies=None):
+    self.aa_list=['D','E','R','K','H','S','T','N','Q','G','P','Y','W','V','I','L','M','F','A','C']
+    self.aa_index_dict={}
+    for i,aa in enumerate(self.aa_list):
+      self.aa_index_dict[aa]=i
+    self.aa_frequencies={"A": 0.09398206173005932, "C": 0.014640847892538094, "E": 0.06167898538341536, "D": 0.057537026726662464, "G": 0.07661345622483157, "F": 0.04125062078013584, "I": 0.05988964354148949, "H": 0.023180301946789893, "K": 0.048775277841393995, "M": 0.022045733851907126, "L": 0.09741587583495241, "N": 0.03822084621874947, "Q": 0.03472639486143553, "P": 0.043952778781438454, "S": 0.060691166121181965, "R": 0.05494787451396235, "T": 0.05250697453686419, "W": 0.012220035049334168, "V": 0.07323789898435108, "Y": 0.032486199178507216}
+    self.ncol=ali.GetLength()
+    self.ali_depth=ali.GetCount()
+    nres=len(self.aa_list)
+    self.matrix=npy.zeros([self.ncol,nres])
+    print "generating pssm from alignment with {0} col and {1} sequences".format(self.ncol,self.ali_depth)
+    for i,aa in enumerate(self.aa_list):
+      self.matrix[:,i]=nres*self.aa_frequencies[aa]
+    for i,col in enumerate(ali):
+      for j,aa in enumerate(self.aa_list):
+        self.matrix[i,j]+=str(col).count(aa)
+      self.matrix[i,:]/=float(npy.sum(self.matrix[i,:]))
+    for i,col in enumerate(ali):
+      for j,aa in enumerate(self.aa_list):
+        #print self.matrix[i,j]/self.aa_frequencies[aa]
+        self.matrix[i,j]=math.log(self.matrix[i,j]/self.aa_frequencies[aa])
+
+  def ScoreSequence(self,s,start_index):
+    score=0
+    for i,c in enumerate(s):
+      if not c in self.aa_index_dict:continue
+      score+=self.matrix[i+start_index,self.aa_index_dict[c]]
+    return score
+
+
+
 
