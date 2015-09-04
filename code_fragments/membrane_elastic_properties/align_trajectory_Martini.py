4f69d1c46dfd51b9247ed168aa27e23099cbd248
Niklaus Johner <niklaus.johner@unibas.ch>
2015-09-04 18:31:54 +0200

    Added code fragments with typical scripts to align trajectories and calculate membrane elastic properties.

---
+++ code_fragments/membrane_elastic_properties/align_trajectory_Martini.py
@@ -0,0 +1,99 @@
+"""
+Code fragment written by Niklaus Johner (niklaus.johner@a3.epfl.ch)
+
+This example presents the typical steps used to align a trajectory of a complex
+lipidic phase. This is a slow process as an optimization is required for each frame.
+If you are working with a lipid bilayer, there are much faster ways of aligning 
+your trajectory (see align_trajectory_bilayer.py).
+
+PBC are treated by replicating the simulation box. The new chains created for
+this purpose will have the same chain name as the ones from the simulation box
+with a different number added at the end of the name for each replicate of the box.
+For example if you have a chain A, and we double the linear size of the system,
+giving 8 replicates of the simulation box, you will end up with chains A,A1,A2,A3,A4,A5,A6,A7.
+It is therefore important that all your chains have names shorter than 4 characters,
+as otherwise you will get chain names with 5 characters which will not be handled properly
+when saving and loading a PDB.
+
+This script will output files to the outdir, replacing files with the same name if present.
+The output files will all start with the "fname_basis"
+The output files are:
+-extended pdb and trajectory, which will have the same names as the loaded trajectory and pdb,
+complemented by "_extended".
+-Aligned pdb and trajectory, which will have the same names as the loaded trajectory and pdb,
+complemented by "_aligned".
+-several other files...
+"""
+from ost import *
+import os,sys
+sys.path.append("../") #Set path to the python modules
+import align_traj_on_density
+import trajectory_utilities
+
+
+
+indir="path/to/input directory"
+outdir="path/to/output directory"
+pdbname="pdbname"
+trajname="trajname"
+fname_basis="MySystem_"
+
+
+#Selection whose center of mass is used as center for the wrapping of the extended simulation.
+wrap_sele="cname=A"
+#selection used to calculate the density map of waters.
+#This should contain all the water molecules of the extended simulation box
+density_sele="rname=W" 
+#selection for which the overlap with the water density will be maximised.
+#This should contain only the water molecules from the initial simulation box
+water_sele2="rname=W and cname=A"
+#selection for which the overlap with the water density will be minimised.
+#This should contain everything in the initial simulation box except the water molecules.
+memb_sele="rname!=W and cname=A"
+
+
+#First we load the structure and trajectory
+p=io.IOProfile(dialect='CHARMM',fault_tolerant=True)
+eh=io.LoadPDB(os.path.join(indir,pdbname+".pdb"),profile=p)
+t=io.LoadCHARMMTraj(eh,os.path.join(indir,trajname+".dcd"),stride=1)
+
+# As this is a coarse grained trajectory, atoms are not properly recognised so that masses
+# were not set when loading the structure. We need to set the masses and element to make sure
+# that center of masses as well as the algorithm calculating the density used to determine 
+# the membrane interface will work properly.
+for a in eh.atoms:
+  a.SetElement('Ge')#Has the correct atomic weight
+  a.SetRadius(2.35)
+  a.SetMass(72)
+
+###################################
+#We extend the trajectory
+#1. We get the unit cell vectors
+vecs_list=[t.GetFrame(i).GetCellVectors() for i in range(t.GetFrameCount())]
+#2. vecs_list contains for each frame a Vec3List with 3 Vec3 in it corresponding
+#   to the three unit cell vectors. We add the vectors to the other neighboring unit cells
+#   for each frame.
+for vl in vecs_list:vl.extend([vl[0]+vl[1],vl[0]+vl[2],vl[1]+vl[2],vl[0]+vl[1]+vl[2]])
+#3. We extend the trajectory to its neighboring unit cells. The last argument is the mutliplicative
+#   factors that will be applied to cell dimensions in the information in each frame
+extended_t=trajectory_utilities.ExtendTrajectoryToNeighboringUnitCells(t,vecs_list,(2,2,2))
+#4. We wrap the trajectory around the central unit cell
+cm=mol.alg.AnalyzeCenterOfMassPos(t,eh.Select(wrap_sele))
+trajectory_utilities.WrapTrajectoryInPeriodicCell(extended_t,cm)
+#5. We extract the entity linked to the trajectory
+#   and then set its positions to the first frame of the trajectory.
+#   This way, when we save the trajectory and structure, the structure will have the positions of the first frame.
+extended_eh=extended_t.GetEntity()
+extended_t.CopyFrame(0)
+io.SaveCHARMMTraj(extended_t,os.path.join(outdir,fname_basis+'extended.pdb'),os.path.join(outdir,fname_basis+'extended.dcd'),profile=p)
+
+
+####################################
+#Now we make the alignment. This will take some time as it is an optimization procedure.
+cm=extended_eh.Select(wrap_sele).GetCenterOfMass()
+xl=align_traj_on_density.AlignTrajectoryOnFirstFrame(extended_t,density_sele,water_sele,memb_sele,\
+                                -cm,True,cm,outdir,fname_basis,0,False)
+
+
+
+
---
+++ code_fragments/membrane_elastic_properties/lipid_elastic_properties_Martini.py
@@ -0,0 +1,207 @@
+"""
+Example written by Niklaus Johner (niklaus.johner@a3.epfl.ch)
+
+This example represents a typical script used to calculate the membrane elastic properties from a coarse-grained Martini simulation.
+As input you need an aligned trajectory (see the align_trajectory_Martini.py for an example of how to align a trajectory).
+
+"""
+from ost import *
+import os,sys
+import numpy as npy
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+sys.path.append("../") #Set path to the python modules
+import lipid_analysis,trajectory_utilities
+
+
+indir="path/to/input directory"
+outdir="path/to/output directory"
+pdbname="pdbname"
+trajname="trajname"
+filename_basis='tilt&splay_'
+
+#We have to define several parameters:
+# 1. The list containing the residue names of the different lipids in the system.
+#    For example if the system contains DOP (DOPC) and CHO (cholesterol).
+lipid_names=['CHO','DOP']
+
+# 2. Define the residue name of the water molecules.
+#    This is used together with the lipid names to calculate the density maps
+#    of water and lipids used to extract the membrane interface and orient the normals on the interface.
+water_name='W'
+
+# 3. For each lipid type, we define the selection for headgroups and tails which
+#    which are used to determine the director vectors.
+#    We also need a selection that will be used to calculate distances between lipids. 
+#    This should be atoms lying at the neutral plane.
+head_group_dict={'DOP':'aname=PO4,GL1,GL2','CHO':'aname=R1'}
+tail_dict={'DOP':'aname=C5A,C5B','CHO':'aname=R5'}
+distance_sele_dict={'DOP':'aname=C1*','CHO':'aname=R1'}
+
+# 4. We define the different cutoffs used by the algorithms. 
+#    We give here good default values, but more information can be found
+#    in the documentation
+#max distance between two lipids to be considered in the splay calculation
+distance_cutoff=10.0 
+#max tilt angle with respect to normal for splay calculations
+angle_cutoff=0.175 
+# radius of area used to calculate the normals to the membrane interface. If too small, normals will be noisy.
+within_size_normals=10.0
+#Density cutoff used when calculating the interface.
+#This can be set to 0, but calculations will be slower.
+density_cutoff=0.3
+#Stride used when calculating the water and lipid densities.
+#When set to 1, all frames are considered, which can be slow.
+density_stride=1
+
+#5. We have to define for which lipids we want to make the analysis.
+#   Typically the trajectory being analysed comprises several replicates
+#   of the simulation system, which is used to treat periodic boundary conditions properly.
+#   But we typically only want to analyse one of the replicas. In this example it has chain name A.
+to_analyze="cname=A and rname=DOP,CHO"
+to_ignore="cname!=A and rname=DOP,CHO"
+
+#7. Other parameters
+prot_sele=None
+sele_dict={}
+
+
+###########################################
+#Now starts the analysis
+#Typically the rest of the script does not have to be modified
+###########################################
+
+#First we load the structure and trajectory
+p=io.IOProfile(dialect='CHARMM',fault_tolerant=True)
+eh=io.LoadPDB(os.path.join(indir,pdbname+".pdb"),profile=p)
+t=io.LoadCHARMMTraj(eh,os.path.join(indir,trajname+'.dcd'),stride=1)
+
+# As this is a coarse grained trajectory, atoms are not properly recognised so that masses
+# were not set when loading the structure. We need to set the masses and element to make sure
+# that the algorithm calculating the density used to determine the membrane interface will work properly.
+for a in eh.atoms:
+  a.SetElement('Ge')#Has the correct atomic weight
+  a.SetRadius(2.35)
+  a.SetMass(72)
+
+# It is generally a good idea to keep only what is needed of the trajectory
+# to reduce the number of atoms and speed up the calculations.
+sele="rname={0}".format(water_name)
+for l in lipid_names:sele+=" or (rname={0} and {1})".format(l,head_group_dict[l])
+for l in lipid_names:sele+=" or (rname={0} and {1})".format(l,tail_dict[l])
+for l in lipid_names:sele+=" or (rname={0} and {1})".format(l,distance_sele_dict[l])
+t=t.Filter(eh.Select(sele))
+eh=t.GetEntity()
+
+
+#   Periodic boundaries are best treated by replicating the simulation box around
+#   the original unit cell and then calculating tilts and splays only for lipids from
+#   the original unit cell, but using the surrounding unit cells to find all neighbors 
+#   of a lipid for the splay calculation and to avoid boundary effects on the interfaces
+#   and hence on the normal vectors used both for the tilt and splay calculations.
+#   We therefore need to tell the function which lipids belong to the central unit cell.
+#   This is done by setting a bool property for the corresponding residues.
+
+tilt_bool_prop='do_tilt'
+splay_bool_prop='do_splay'
+
+
+v=eh.Select(to_analyze)
+for r in v.residues:
+  r.SetBoolProp(tilt_bool_prop,True)
+  r.SetBoolProp(splay_bool_prop,True)
+v=eh.Select(to_ignore)
+for r in v.residues:
+  r.SetBoolProp(tilt_bool_prop,False)
+  r.SetBoolProp(splay_bool_prop,False)
+
+
+#  We calculate the tilts and splays:
+(lipid_tilt_dict,lipid_normal_dict,splay_dict,b_eh)=lipid_analysis.AnalyzeLipidTiltAndSplay(t,
+  lipid_names,head_group_dict,tail_dict,distance_cutoff,within_size_normals,distance_sele_dict,water_name,
+  outdir,density_cutoff,prot_sele,density_stride,tilt_bool_prop,splay_bool_prop,filename_basis,sele_dict)
+
+
+########################################################
+# Now we analyze the lipid tilts and splays, fitting the corresponding analytical functions
+# to extract the elastic constants. See documentation and papers cited therein for more information.
+# We will make one fit for each lipid type, and then calculate the overall tilt modulus
+# By taking a weighted average of the individual contributions.
+
+# The number of bins used when making the histograms for the tilts
+nbins=100
+
+# Name of the file where the tilt constants will be written
+outfile=open(os.path.join(outdir,'tilt_constants.txt'),'w')
+
+# Lists where we will store the tilt moduli, the associated uncertainty and the number of lipids of each type.
+k_list=FloatList()
+deltak_list=FloatList()
+nl_list=FloatList()
+for lipid_name in lipid_names:
+  fname=lipid_name
+  
+  #We make a list containing all the tilts for one type of lipids.
+  #In the dictionary, there is one entry for each lipid so we have to concatenate all of them into one list.
+  tilt_list=FloatList()
+  for el in lipid_tilt_dict['all'][lipid_name][0]:tilt_list.extend(el)
+
+  #Now we make the fit. This will generate plots written to outdir.
+  k,dk,kl=lipid_analysis.FitTiltDistribution(tilt_list,outdir=outdir,filename_basis=fname,title_complement='for '+lipid_name,nbins=nbins)
+  print "Tilt modulus for {0} is k={1:1.2f} +/- {2:1.2f}.".format(lipid_name,k,dk)
+
+  #We write out the tilt modulus into the file
+  outfile.write(' '.join([lipid_name,str(round(kl[0],1)),str(round(dk,1))])+'\n')
+  
+  #Keep the tilt modulus, error and number of lipids of that kind to calculate the overall modulus later.
+  k_list.append(k)
+  deltak_list.append(dk)
+  nl_list.append(len(tilt_list))
+
+#Now we calculate the overall tilt modulus
+ntot=npy.sum(nl_list)
+k=npy.sum([ni/ki for ni,ki in zip(nl_list,k_list)])/ntot
+k=1./k
+dk=npy.sum([((k**2.0)/(ki**2.0)*ni/ntot)**2.0*(dki**2.0) for ni,ki,dki in zip(nl_list,k_list,deltak_list)])
+dk=npy.sqrt(dk)
+#write it out
+outfile.write(' '.join(['agregated',str(round(k,1)),str(round(dk,1))])+'\n')
+outfile.close()
+#Close the generated plots
+for i in range(10):plt.close()
+
+# Now we fit the splays to extract the bending rigidities.
+# The procedure is the same as for the tilts
+# Except that we need the area per lipid to calculate the bending rigidity from the splays
+# In this example the area per lipid is 49.5 A^2. For a lipid bilayer the area can be obtained
+# Using the AnalyzeAreaPerLipid function.
+lipid_area=49.5
+
+outfile=open(os.path.join(outdir,'splay_constants.txt'),'w')
+k_list=FloatList()
+nl_list=FloatList()
+deltak_list=FloatList()
+nbins=100
+for key in splay_dict['all']:
+  fname=key
+  splay_list=[el[0] for el in splay_dict['all'][key]]
+  w=npy.std(splay_list)
+  x_range=[-4*w,4*w]
+  k,dk,kl=lipid_analysis.FitSplayDistribution(splay_list,lipid_area,outdir=outdir,filename_basis=fname,title_complement='for '+key,nbins=nbins,x_range=x_range)
+  print "Splay modulus for {0} is k={1:1.2f} +/- {2:1.2f}.".format(key,k,dk)
+  outfile.write(' '.join([key,str(round(kl[0],1)),str(round(dk,1))])+'\n')
+  k_list.append(k)
+  deltak_list.append(dk)
+  nl_list.append(len(splay_list))
+ntot=npy.sum(nl_list)
+k=npy.sum([ni/ki for ni,ki in zip(nl_list,k_list)])/ntot
+k=1./k
+dk=npy.sum([((k**2.0)/(ki**2.0)*ni/ntot)**2.0*(dki**2.0) for ni,ki,dki in zip(nl_list,k_list,deltak_list)])
+dk=npy.sqrt(dk)
+outfile.write(' '.join(['agregated',str(round(k,1)),str(round(dk,1))])+'\n')
+outfile.close()
+
+#Close the generated plots
+for i in range(10):plt.close()
+
