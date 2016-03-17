52fe5b1dff14f9654a40e4f3e5dc0972b168606e
Niklaus Johner <niklaus.johner@unibas.ch>
2016-03-17 08:52:52 +0100

    Added License information

--- sequence_alg.py
+++ sequence_alg.py
@@ -1,4 +1,23 @@
+#------------------------------------------------------------------------------------------------
+#This file is part of the ost_pymodules project (https://github.com/njohner/ost_pymodules).
+#
+#Copyright 2015 Niklaus Johner
+#
+#ost_pymodules is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Lesser General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#ost_pymodules is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Lesser General Public License for more details.
+#
+#You should have received a copy of the GNU Lesser General Public License
+#along with ost_pymodules.  If not, see <http://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------------------------------
 """
+
 .. codeauthor:: Niklaus Johner <niklaus.johner@a3.epfl.ch>
 
 This module contains basic functions to work with sequences, notably to find motifs,
